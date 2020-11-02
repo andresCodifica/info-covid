3e521786c4230a6715b57d8daa379bde8a1e1bc0
Jhair Andres Escobar Rios <jaescobarr@losunos.net>
2020-11-01 20:15:18 -0500

    estructura del proyecto

---
+++ src/main/java/com/co/covid/controller/ViewController.java
@@ -0,0 +1,19 @@
+package com.co.covid.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+	
+	@GetMapping("/")
+	public String Index() {
+		return "index";
+	}
+	@GetMapping("/contacto")
+	public String Contacto() {	
+		return "contacto";
+	}
+	
+	
+}
